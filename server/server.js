7927c53e8fb9045b5afcd79d2a0bdd0716ca2cc0
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-11-01 21:07:45 +0700

    dsa

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql2");
 
-const con = mysql.createPool({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "SharkFangs8899",
@@ -14,7 +14,7 @@ app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -28,28 +28,59 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   if (err) throw err
 //   console.log("Connection Successful!");
 // });
-
 app.post("/api/signin/post", (req, res) => {
+  const { email, user_name, password } = req.body;
+  const sqlInsert =
+    "INSERT INTO user (email, user_name, password) VALUES (?, ?, ?)";
+  db.query(sqlInsert, [email, password, user_name], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(result);
+    }
+  });
+});
+app.get("/api/signin/get", (req, res) => {
   console.log(req.body);
   const email = req.body.email;
   const user_name = req.body.user_name;
   const password = req.body.password;
 
-  const query =
-    "INSERT INTO `user` (`email`, `user_name`, `password`) VALUES (?, ?, ?);";
+  const query = "SELECT * from `user`";
 
-  con.query(query, [email, user_name, password], (err, result) => {
+  db.query(query, [email, user_name, password], (err, result) => {
     console.log(err, result);
     if (err) {
       res.send(err);
     } else {
       console.log("success");
-      res.send("successfully added to db");
+      res.send(result);
     }
     // console.log(err);
     //console.log(result)
   });
 });
+// app.post("/api/signin/post", (req, res) => {
+//   console.log(req.body);
+//   const email = req.body.email;
+//   const user_name = req.body.user_name;
+//   const password = req.body.password;
+
+//   const query =
+//     "INSERT INTO `user` (`email`, `user_name`, `password`) VALUES ('wincent, 'asddcsa', 'fdanlj');";
+
+//   con.query(query, [email, user_name, password], (err, result) => {
+//     console.log(err, result);
+//     if (err) {
+//       res.send(err);
+//     } else {
+//       console.log("success");
+//       res.send("successfully added to db");
+//     }
+//     // console.log(err);
+//     //console.log(result)
+//   });
+// });
 
 app.get("/", (req, res) => {
   console.log("Responding to root route");
