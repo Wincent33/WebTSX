7cde7b549c798b17557265ed99b09e7cb1ec9785
Wincent33 <46631761+Wincent33@users.noreply.github.com>
2022-11-15 11:09:17 +0700

    vfsdc

--- client/src/Components/DetailPageComponents/Detail3/index.tsx
+++ client/src/Components/DetailPageComponents/Detail3/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import ImageGallery from "react-image-gallery";
 import "./style.scss";
 const Detail3 = () => {
-  return <div className="fitur">Fitur</div>;
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+  return (
+    <div className="fitur">
+      <ImageGallery
+        items={images}
+        // showFullscreenButton={false}
+        thumbnailPosition="right"
+        showPlayButton={false}
+        autoPlay={true}
+        showNav={false}
+      />
+    </div>
+  );
 };
 
 export default Detail3;
